a8a96e366623db9d1d7031d9a256e1416f4418d6
Mvngo1 <stuart.moss2006@gmail.com>
2024-08-05 19:43:21 -0400

    Update mod.js
    changed name, started project

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "Incremental^2",
+	id: "incrementalsquared",
+	author: "Mvngo",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
